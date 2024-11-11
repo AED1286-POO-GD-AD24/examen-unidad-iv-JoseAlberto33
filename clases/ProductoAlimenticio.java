5934ecc4fca7a004a91579af0703dadd8a9afff7
JoseAlberto33 <147731256+JoseAlberto33@users.noreply.github.com>
2024-11-11 16:59:37 +0000

    FINAL

--- clases/ProductoAlimenticio.java
+++ clases/ProductoAlimenticio.java
@@ -1,4 +1,18 @@
 package clases;
-// Clase ProductoAlimenticio que extienda de Producto
 
+public class ProductoAlimenticio extends Producto {
 
+    public ProductoAlimenticio(String nombre, double precio) {
+        super(nombre, precio);
+    }
+
+    @Override
+    public String getNombre() {
+        return super.getNombre();
+    }
+
+    @Override
+    public double getPrecio() {
+        return super.getPrecio();
+    }
+}
